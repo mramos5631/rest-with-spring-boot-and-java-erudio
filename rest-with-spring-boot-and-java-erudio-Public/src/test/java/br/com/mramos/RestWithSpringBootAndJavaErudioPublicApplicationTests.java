e1209cdf8e1458abcd56b146cfec08e47b565f8e
Mauro A Ramos <mauro.ramos2007@gmail.com>
2024-10-28 11:54:52 -0300

    primeiras funcionalidades

---
+++ rest-with-spring-boot-and-java-erudio-Public/src/test/java/br/com/mramos/RestWithSpringBootAndJavaErudioPublicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mramos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaErudioPublicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
